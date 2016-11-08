7e34fe0d960bf545d7d749c0c3266f98b0af622b
Caroline.Collaco <carolinecollaco@gmail.com>
2016-11-08 19:06:34 -0200

    Elsif

---
+++ Elsif.rb
@@ -0,0 +1,7 @@
+if 1 > 1
+    print "Eu estou impresso porque um é menor que dois!"
+elsif
+    print "Eu estou impresso porque um é igual a um!"
+else
+    print "Isso significa que eu vou ser impresso!"
+end
