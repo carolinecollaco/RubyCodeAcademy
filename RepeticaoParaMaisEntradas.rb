99779be93f0bee87def933c2cb6c29e48581087e
Caroline.Collaco <carolinecollaco@gmail.com>
2016-11-08 18:39:48 -0200

    Repetição para Mais Entradas

---
+++ RepeticaoParaMaisEntradas.rb
@@ -0,0 +1,11 @@
+print "Qual é o seu nome?"
+first_name = gets.chomp
+
+print "Qual é o seu sobrenome?"
+last_name = gets.chomp
+
+print "De que cidade você é?"
+city = gets.chomp
+
+print "De que estado você é (sigla)?"
+state = gets.chomp
