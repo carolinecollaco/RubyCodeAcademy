a6aac0ca4c8c98d40301d30824c9a51eba174fca
Caroline.Collaco <carolinecollaco@gmail.com>
2016-11-09 17:30:20 -0200

    Configurando o Ramo 'Else'

---
+++ ConfigurandoORamoElse.rb
@@ -0,0 +1,9 @@
+print "Thtring, por favor!: "
+user_input = gets.chomp
+user_input.downcase!
+
+if user_input.include? "s"
+    user_input.gsub!(/s/, "th")
+else 
+    print"Não tem s na sua string"
+end
