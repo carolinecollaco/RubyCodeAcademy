554258f2cdec3b789b78fce634da284e568350f0
Caroline.Collaco <carolinecollaco@gmail.com>
2016-11-11 21:39:57 -0200

    Crie um Histograma e Você Sabe o que Fazer

---
+++ Hashes.rb
@@ -0,0 +1,3 @@
+my_hash = Hash.new
+my_hash["justin"] = "dog"
+my_hash["dean"]= "cat"
---
+++ IterandoEmEmHash.rb
@@ -0,0 +1,9 @@
+lunch_order = {
+  "Ryan" => "sopa",
+  "Eric" => "hamburger",
+  "Jimmy" => "sanduíche",
+  "Sasha" => "salada",
+  "Cole" => "taco"
+}
+
+lunch_order.each {|x,y| puts "#{y}"}
