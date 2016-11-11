485ce0693dfe1d824619ff42ebab51949dd3cd06
Caroline.Collaco <carolinecollaco@gmail.com>
2016-11-11 19:28:51 -0200

    Iterando em Hashes

---
+++ IterandoEmHashes.rb
@@ -0,0 +1,10 @@
+secret_identities = {
+  "O Batman" => "Bruce Wayne",
+  "Superman" => "Clark Kent",
+  "Mulher Maravilha" => "Diana Prince",
+  "Freakazoid" => "Dexter Douglas"
+}
+
+secret_identities.each do |heroi,pessoa|
+    puts "#{heroi}: #{pessoa}"
+ end   
