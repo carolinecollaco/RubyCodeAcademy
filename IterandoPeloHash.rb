a3cf3a9604f2183b2df63579761f962d1c49f939
Caroline.Collaco <carolinecollaco@gmail.com>
2016-11-13 22:01:36 -0200

    Iterando pelo Hash

---
+++ IterandoPeloHash.rb
@@ -0,0 +1,9 @@
+puts "Entre com o texto: "
+text = gets.chomp
+
+words = text.split(" ")
+frequencies = Hash.new(0)
+words.each {|word| frequencies[word] += 1}
+frequencies = frequencies.sort_by {|frequency, count|}
+frequencies.reverse!
+frequencies.each { |word, frequency| puts word + " " + frequency.to_s}
