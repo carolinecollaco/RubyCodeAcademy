26f176c95b5cfb12b82ead846ff0816cf66dd8fa
Caroline.Collaco <carolinecollaco@gmail.com>
2016-11-14 17:51:43 -0200

    Crie Seu Próprio

---
+++ CrieSeuProprio1.rb
@@ -0,0 +1,10 @@
+# Defina seu método abaixo!
+def greeting
+    puts "Olá, tudo bem!"
+end
+
+
+# Defina seu método acima desta linha.
+
+greeting # Ignore isso por enquanto. Explicaremos
+         # isso no próximo exercício!
