c3b47da9dbe4b02123ffe549f32d3984d8a05693
Caroline.Collaco <carolinecollaco@gmail.com>
2016-11-14 18:45:31 -0200

    Fundações

---
+++ Fundacoes.rb
@@ -0,0 +1,6 @@
+# biblioteca de algoritmos de ordenação
+books = ["Charlie and the Chocolate Factory", "War and Peace", "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+
+# Como fazemos para ordenar os livros, usando sort!, em ordem alfabética? (Dicas, dicas)
+
+books.sort!
