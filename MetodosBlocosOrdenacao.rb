07c99029d319dbd2b4ed1969f79921b15245a8cb
Caroline.Collaco <carolinecollaco@gmail.com>
2016-11-14 17:35:03 -0200

    Métodos, Blocos, e Ordenação

---
+++ MetodosBlocosOrdenacao.rb
@@ -0,0 +1,20 @@
+def prime(n)
+  puts "Isso não é um inteiro." unless n.is_a? Integer
+  is_prime = true
+  for i in 2..n-1
+    if n % i == 0
+      is_prime = false
+    end
+  end
+  if is_prime
+    puts "#{n} é primo!"
+  else
+    puts "#{n} não é primo."
+  end
+end
+
+prime(2)
+prime(9)
+prime(11)
+prime(51)
+prime(97)
